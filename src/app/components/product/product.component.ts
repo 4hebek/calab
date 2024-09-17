474d577ddfc89b52dbfffe94d48ab8ec9faba76e
kaz-hab <42947422+4hebek@users.noreply.github.com>
2024-09-17 16:17:51 +0100

    Using directives

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { CurrencyPipe } from '@angular/common';​
 import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { ZoomOnHooverDirective } from '../../directives/zoom-on-hoover.directive';
+import { TooltipDirective } from '../../directives/tooltip.directive';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe, TruncatePipe],
+  imports: [CurrencyPipe, TruncatePipe, ZoomOnHooverDirective, TooltipDirective],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -16,6 +18,5 @@ export class ProductComponent {
 
   onAddToCartClicked(){
     this.addToCartEvent.emit(this.product);
-}
-
+  }
 }
